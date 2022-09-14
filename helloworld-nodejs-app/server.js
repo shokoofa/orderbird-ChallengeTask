645fc5fdfa658faa941e4881dbd3fa6b52d2eb3c
shokoofa <shokoofa.hosseini@gmail.com>
2022-09-14 23:00:47 +0430

    Create server.js

---
+++ helloworld-nodejs-app/server.js
@@ -0,0 +1,14 @@
+// importing express framework
+    const express = require("express");
+
+    const app = express();
+
+    // Respond with "hello world" for requests that hit our root "/"
+    app.get("/", function (req, res) {
+     return res.send("Hello World");
+    });
+
+    // listen to port 8080 by default
+    app.listen(process.env.PORT || 8080, () => {
+      console.log("Server is running");
+    });
